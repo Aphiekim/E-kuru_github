0a095aa9d86cfb615b0a5b51f2edea27c2c2d0e8
AphieKim <aphiekim@gmail.com>
2021-03-20 13:56:34 +0900

    21/03/20 채팅 유저 타입 변경

--- ekuru/src/main/java/com/scit/ekuru/controller/UserController.java
+++ ekuru/src/main/java/com/scit/ekuru/controller/UserController.java
@@ -174,14 +174,15 @@ public class UserController {
 		String id = (String)session.getAttribute("userId");
 		UserVO uservo = service.selectUserTest(id);
 		ChatVO chatvo = null;
-		//System.out.println(uservo);
-		if(uservo.getUserType().equals("1")) {
+		System.out.println(uservo);
+		
+		if(uservo.getUserType().equals("0")) {
 			chatvo = service.selectChUser1();
 		}else {
 			chatvo = service.selectChUser2();
 		}
 
-		//System.out.println(chatvo);
+		System.out.println(chatvo);
 
 		ArrayList<HashMap<Object, Object>> chatroomlist = service.selectChatRoom(chatvo);
 		model.addAttribute("chatroomlist", chatroomlist);
